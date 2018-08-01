f5ae2dd33631f0ce32d977bc380caf0b725b7010
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-08-01 12:08:47 -0700

    Add Adam optimizer

--- torch/optim/__init__.pyi
+++ torch/optim/__init__.pyi
@@ -2,10 +2,13 @@
 from typing import Dict, Any, List
 
 class Optimizer:
-    pass
-
-class SGD(Optimizer):
     def __init__(self, params : Dict[str, Any], lr : float = ...) -> None: ...
     def zero_grad(self) -> None: ...
     def step(self) -> None: ...
     param_groups = ... # type: List[Dict[str, Any]]
+
+class SGD(Optimizer):
+    pass
+
+class Adam(Optimizer):
+    pass
