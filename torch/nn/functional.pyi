af5907be2d142a09d4f08948957b00fc2bd2264b
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-10-20 15:27:16 -0700

    Add one_hot, where, and full_like

--- torch/nn/functional.pyi
+++ torch/nn/functional.pyi
@@ -1,8 +1,11 @@
 
-from torch import Tensor, FloatTensor
+from torch import Tensor, FloatTensor, LongTensor
 from typing import overload
 
 @overload
 def relu(t : FloatTensor) -> FloatTensor : ...
 @overload
 def relu(t : Tensor) -> Tensor : ...
+
+
+def one_hot(t: LongTensor, n: int) -> FloatTensor: ...
